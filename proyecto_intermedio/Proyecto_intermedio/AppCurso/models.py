cdcb81a8288ebb827415655ba365ebe7a140b0f0
chiara-facal <chiarafacal@github.com>
2023-03-08 21:03:51 -0300

    models agregados

--- proyecto_intermedio/Proyecto_intermedio/AppCurso/models.py
+++ proyecto_intermedio/Proyecto_intermedio/AppCurso/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+
+class Alumno(models.Model):
+    nombre_completo = models.CharField(max_length=40)
+    curso = models.CharField(max_length=30)
+    email = models.CharField(max_length=40)
+    fecha_de_registro = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Alumno: {self.nombre_completo}. Curso:{self.curso}'
+
+class Profesor(models.Model): 
+    nombre_completo = models.CharField(max_length=40)
+    curso = models.CharField(max_length=30)
+    rol = models.CharField(max_length=15)
+    profesion = models.CharField(max_length=30)
+
+    def __str__(self):
+        return f'Profesor: {self.nombre_completo}. Curso: {self.curso}'
+
+class Entregable(models.Model):
+    nombre_alumno = models.CharField(max_length=40)
+    nombre_entregable = models.Charfiled (max_length=30)
+    curso = models.CharField(max_length=30)
+    fecha_de_entrega = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return "Su devolución será realizada pronto"
