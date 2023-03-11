c14adfeb6dc60ea23b01e99db80faeaaf27c9a5a
chiara-facal <chiarafacal@github.com>
2023-03-11 15:24:45 -0300

    Listado en las views

--- proyecto_intermedio/Proyecto_intermedio/AppCurso/views.py
+++ proyecto_intermedio/Proyecto_intermedio/AppCurso/views.py
@@ -1,13 +1,23 @@
 from django.shortcuts import render
+from AppCurso.models import Alumno, Profesor, Entregable
 
 def index(request):
     return render(request, "AppCurso/index.html")
 
 def registro_alumnos(request):
-    return render(request, "AppCurso/alumnos.html")
+
+    alumnos_registrados = Alumno.objects.all()
+
+    return render(request, "AppCurso/alumnos.html", {"alumnos_registrados": alumnos_registrados})
 
 def registro_profesores(request):
-    return render(request, "AppCurso/profesores.html")
+
+    profesores_registrados = Profesor.objects.all()
+
+    return render(request, "AppCurso/profesores.html", {"profesores_registrados": profesores_registrados})
 
 def entregables(request):
-    return render(request, "AppCurso/entregables.html")
+
+    entrega_trabajo = Entregable.objects.all()
+
+    return render(request, "AppCurso/entregables.html", {"entrega_trabajo": entrega_trabajo})
